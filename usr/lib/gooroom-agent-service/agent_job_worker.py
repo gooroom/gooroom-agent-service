e29cc6c10a8af87202e0684683778d9228239ed7
hmkim <hmkim@gooroom.kr>
2017-09-07 10:12:18 +0900

    (bug fix) task_rsp -> task
    Change-Id: I5927394e4c2434e591066ee554b78f5e27969253

--- usr/lib/gooroom-agent-service/agent_job_worker.py
+++ usr/lib/gooroom-agent-service/agent_job_worker.py
@@ -221,12 +221,12 @@ class AgentJobWorker(threading.Thread):
 
         for task in job:
             task_rsp_list.append(self.do_task(task))
-            if task[J_MOD][J_TASK][J_OUT][J_STATUS] != AGENT_OK:
+            if task[J_MOD][J_TASK][J_OUT][J_STATUS] == AGENT_OK:
+                if task[J_MOD][J_TASK][J_OUT][J_MESSAGE] == SKEEP_SERVER_REQUEST:
+                    task[J_MOD][J_TASK][J_OUT][J_MESSAGE] = ''
+            else:
                 job_status = AGENT_NOK
                 break
-            else:
-                if task_rsp[J_MOD][J_TASK][J_OUT][J_MESSAGE] == SKEEP_SERVER_REQUEST:
-                    task_rsp[J_MOD][J_TASK][J_OUT][J_MESSAGE] = ''
                 
         server_rsp, status_code, err_msg = \
             self.data_center.serverjob_request(task_rsp_list, job_no, job_status)
