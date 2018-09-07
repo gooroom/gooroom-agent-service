6487ac4bf831dcbc436f32192eabc3704a78482c
hmkim <hmkim@gooroom.kr>
2018-09-07 13:08:33 +0900

    modified homefolder-deletion logic
    Change-Id: If9fef9ae1ffa1f0ca2b4c1c72c2dfa7050c30a59

--- usr/lib/gooroom-agent-service/modules/LOCAL.py
+++ usr/lib/gooroom-agent-service/modules/LOCAL.py
@@ -56,7 +56,6 @@ def task_delete_homefolder(task, data_center):
         if 'gooroom-online-account' in splited[4]:
             online_accounts.append(splited[0])
 
-    print('online account=', online_accounts)
     AgentLog.get_logger().error(e)
 
     #현재 로그인한 계정
@@ -75,8 +74,6 @@ def task_delete_homefolder(task, data_center):
 
         if ut_type == 7 and ut_id == ':0':
             logined_accounts.append(ut_user.decode('utf8').strip('\00'))
-
-    print('logined accounts={}'.format(logined_accounts))
 
     #로컬에 존재하는 온라인계정이 로그인 중이 아니라면 삭제 절차
     for online_account in online_accounts:
@@ -99,7 +96,27 @@ def task_delete_homefolder(task, data_center):
                                                             kill_out))
                     AgentLog.get_logger().info('killed process({})'.format(pid))
 
-            #홈디렉토리에 마운트되어 있는 것들을 마운트해제
+            #ecryptfs umount:로그아웃 시 ecryptfs-umount-private 실패를 고려해서
+            #ecryptfs-umount-private를 결과와 상관없이 한 번 실행
+            umount_cmd = subprocess.Popen(
+                ['sudo', 
+                '-u', 
+                online_account, 
+                '/usr/bin/ecryptfs-umount-private'],
+                stdout=subprocess.PIPE)
+            umount_out, umount_err = umount_cmd.communicate()
+            AgentLog.get_logger().info(
+                'ecryptfs umount={} {}'.format(umount_out, umount_err))
+
+            #shield
+            encrypted_fname = 'Access-Your-Private-Data.desktop'
+            if not os.path.isfile(
+                '/home/{}/{}'.format(online_account, encrypted_fname)):
+                raise Exception(
+                    '{} not found in /home/{}'.format(encrypted_fname, online_account))
+                
+            #클라우드에 마운트되어있는 데이터를 보호하기 위해서
+            #홈디렉토리에 마운트되어 있는 것들을 존재한다면 마운트해제
             for fn in os.listdir('/home/{}'.format(online_account)):
                 mounted = os.path.ismount(fn)
 
@@ -108,7 +125,6 @@ def task_delete_homefolder(task, data_center):
                                             ['umount', fn], 
                                             stdout=subprocess.PIPE)
                     umount_out = umount_cmd.communicate()[0]
-                    print(umount_out)
                     if umount_cmd.returncode != 0:
                         raise Exception(
                             'failed to umount({})={}'.format(
@@ -121,13 +137,13 @@ def task_delete_homefolder(task, data_center):
                 ['deluser', '--remove-home', 
                 online_account], 
                 stdout=subprocess.PIPE)
-            deluser_out = deluser_cmd.communicate()[0]
-            print(deluser_out)
+            deluser_out, deluser_err = deluser_cmd.communicate()
             if deluser_cmd.returncode != 0:
                 raise Exception(
-                    'failed to deluser({})={}'.format(
+                    'failed to deluser({})={} {}'.format(
                                                 online_account, 
-                                                deluser_out))
+                                                deluser_out,
+                                                deluser_err))
             AgentLog.get_logger().info('deleted ({})'.format(online_account))
 
             #ecryptfs 디렉토리 삭제
